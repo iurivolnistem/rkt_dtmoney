e22997ca798a29a4be7c1acdde9aee473bbd0a2a
Iuri Volnistem <iurivolnistem@gmail.com>
2023-08-29 19:01:57 -0300

    Tabela de transações criada e estilizada

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,38 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 10.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>29/08/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,90</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/08/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </>
   )
 }
